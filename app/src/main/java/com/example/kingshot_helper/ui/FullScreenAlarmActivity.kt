30d5b0ac47a7f36ed27f820029b7a3e816eda85b
aminbehrooz92-eng <amin.behrooz92@gmail.com>
2025-10-19 02:39:48 +0330

    Update FullScreenAlarmActivity.kt

--- app/src/main/java/com/example/kingshot_helper/ui/FullScreenAlarmActivity.kt
+++ app/src/main/java/com/example/kingshot_helper/ui/FullScreenAlarmActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kingshot_helper.ui
 
+import androidx.compose.ui.res.stringResource
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
